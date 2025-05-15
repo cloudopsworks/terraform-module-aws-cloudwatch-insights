0baa24d3ed930a61206a584a07e514b1fd595fcd
Cristian Beraha <cristian@cloudops.works>
2025-05-15 19:18:29 -0300

    Add CloudWatch event rule resource with configurable settings
    This commit introduces the `aws_cloudwatch_event_rule` resource, allowing dynamic configuration based on the `settings` variable. A new `variables-cloudwatch.tf` defines the `settings` variable, and outputs.tf is prepared for future outputs. These changes enhance module flexibility for CloudWatch event rule management.

--- main.tf
+++ main.tf
@@ -4,3 +4,16 @@
 #            Distributed Under Apache v2.0 License
 #
 
+resource "aws_cloudwatch_event_rule" "rule" {
+  for_each = {
+    for item in try(var.settings.event_rules, []) : item.name => item
+  }
+  name                = each.value.name
+  description         = try(each.value.description, null)
+  event_pattern       = jsonencode(each.value.event_pattern)
+  schedule_expression = try(each.value.schedule_expression, null)
+  event_bus_name      = try(each.value.event_bus_name, null)
+  role_arn            = try(each.value.role_arn, null)
+  state               = try(each.value.state, "ENABLED")
+  tags                = local.all_tags
+}
---
+++ variables-cloudwatch.tf
@@ -0,0 +1,11 @@
+##
+# (c) 2024 - Cloud Ops Works LLC - https://cloudops.works/
+#            On GitHub: https://github.com/cloudopsworks
+#            Distributed Under Apache v2.0 License
+#
+
+variable "settings" {
+  description = "Module Cloudwatch Insights configuration"
+  type        = any
+  default     = {}
+}
